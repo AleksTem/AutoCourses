6ab0a50bd19a2d907fd5dfcfc0b01ecb4228e561
Oleksandr Kovalenko <alex.trapper@gmail.com>
2019-04-26 12:25:44 +0300

    Task #04  done

---
+++ HomeWork_04/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace HomeWork_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дайте словечко");
+            string input = Console.ReadLine();
+            LinkedList<char> chars = new LinkedList<char>(input);
+            if (CheckForPalindrome(chars))
+                Console.WriteLine($"Слово \"{input}\" является палиндромом.");
+            else
+                Console.WriteLine($"Слово \"{input}\" не является палиндромом.");
+            Console.ReadKey();
+        }
+
+        private static bool CheckForPalindrome(LinkedList<char> chars)
+        {
+            if (chars.Count <= 1)
+                return true;
+            else if (!chars.First.Value.Equals(chars.Last.Value))
+                return false;
+            else
+            {
+                chars.RemoveFirst();
+                chars.RemoveLast();
+                return CheckForPalindrome(chars);
+            }
+        }
+    }
+}
