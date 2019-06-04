c623dd2afe57d218643da69175d853698e627b17
Oleksandr Kovalenko <alex.trapper@gmail.com>
2019-06-04 18:34:03 +0300

    fix

--- HomeWork_03/Program.cs
+++ HomeWork_03/Program.cs
@@ -8,8 +8,8 @@ namespace HomeWork_03
     {
         static void Main(string[] args)
         {
-            int[] arr = new CustomRange(10).Range;
-            var _sum = Array.FindAll(arr, x => (x % 3 == 0) && (x % 5 != 0)).Sum();
+            long[] arr = new CustomRange(10).Range;
+            long _sum = Array.FindAll(arr, x => (x % 3 == 0) && (x % 5 != 0)).Sum();
             Console.WriteLine($"Sum of numbers that are divisible by 3 but not divisible by 5 is: {_sum}");
             Console.ReadKey();
         }
